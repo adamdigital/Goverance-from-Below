67768d35c68e07e04a210bf2fd2b90851bf9c67a
Adam <adam.brown@digital.justice.gov.uk>
2019-08-16 16:25:11 +0100

    Fix rouge bracket

--- src/components/c-banner.php
+++ src/components/c-banner.php
@@ -12,10 +12,9 @@ $feature_banner_button_text = get_field('feature_banner_button_text');
 $feature_banner_button_url = get_field('feature_banner_button_url');
 
 // Check user has selected an image. If not, hide banner.
-if ( $feature_banner_image_url ) :
-?>
-<!-- banner starts here -->
+if ( $feature_banner_image_url ) : ?>
 
+<!-- banner starts here -->
 <div class="jumbotron text-light d-print-none">
 	<div class="container">
 	<h1 class="display-4"><?php echo $feature_banner_main_title; ?></h1>
@@ -37,11 +36,8 @@ if ( $feature_banner_image_url ) :
 
 <?php endif;
 
-
 // Homepage can't be easily customised with ACF so we're going manual here for now
-if ( is_home() ) :
-
-?>
+if ( is_home() ) : ?>
 <!-- banner starts here -->
 
 <div class="jumbotron text-light d-print-none">
@@ -58,6 +54,5 @@ if ( is_home() ) :
 	// JS resizing background image to block element
  	$(".jumbotron").backstretch("https://governancefrombelow.net/wp-content/uploads/2013/12/bol-chile-2500x470.jpg");
 </script>
-}
 
 <?php endif;
